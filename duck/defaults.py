b0bcfe304ea3a5e8cb9ead2999a178edb8c96d40
Brian Musakwa <digreatbrian@gmail.com>
2025-01-24 03:03:31 +0200

    Duck cleanup for new setup

--- duck/defaults.py
+++
@@ -1,3 +0,0 @@
-
-SSL_DEFAULTS={'certfile':None, 'cert_reqs':0, 'keyfile':None, 'version':2,
-'ca_certs':None ,'supress_ragged_eofs':True, 'ciphers':None}
--- duck/kwarghandler.py
+++
@@ -1,8 +0,0 @@
-#kwarg handler
-'''Handle class keywords in __init__ method .'''
-
-def handle_init_kwargs(obj,kwargs):
-	'''Handle class keywords'''
-	for x in kwargs.keys():
-		exec('obj.{key} = kwargs["{key}"]'.format(key=x))
-	
